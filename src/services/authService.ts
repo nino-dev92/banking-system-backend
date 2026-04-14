a2b294a7f4d54e9aeb1b53ba67ae97c89270383e
ninodev92 <daren2track@gmail.com>
2026-04-14 06:16:47 +0100

    added online connection string

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -28,7 +28,7 @@ export const login = async (username: string, password: string) => {
   const accessToken = jwt.sign(
     { id: user._id, username: user.username },
     process.env.ACCESS_TOKEN_SECRET as string,
-    { expiresIn: "10s" },
+    { expiresIn: "5m" },
   );
 
   const refreshToken = jwt.sign(
